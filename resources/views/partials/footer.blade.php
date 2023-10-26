bd232f4174f3c4fb6a97722b4d75bf8cc03db686
valeriodipaolo1997 <dipaolovalerio80gmail.com>
2023-10-26 16:34:19 +0200

    add content in footer.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,160 @@
-<h1>FOOTER</h1>
+<footer>
+
+    <div class="footer_top">
+        <div class="container">
+            <div class="row">
+                <div class="col-6 py-5">
+                    <div class="d-flex gap-5">
+
+                        <div>
+                            <h3>dc comics</h3>
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a href="#">link </a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+                            </ul>
+
+
+                            <h3>shop</h3>
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a href="#">link </a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div>
+                            <h3>dc</h3>
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a href="#">link </a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div>
+                            <h3>sites</h3>
+                            <ul class="list-unstyled">
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+
+                                <li>
+                                    <a href="#">link</a>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="col-6 logo">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer_bottom">
+        <div class="container d-flex justify-content-between h-100">
+
+            <div class=" d-flex align-items-center">
+                <a href="##">
+                    <button class="btn btn-outline-primary text-uppercase text-white rounded-0 fs-5">
+                        sign-up now!
+                    </button>
+                </a>
+            </div>
+
+            <div id="socials" class=" d-flex align-items-center gap-4">
+                <h3>follow us</h3>
+                <a>
+                    <img src="" alt="">
+                </a>
+            </div>
+
+
+        </div>
+
+    </div>
+
+</footer>
